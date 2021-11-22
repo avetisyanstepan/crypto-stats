6db7e24415f1a379afc5d7e0c4492c5d54359eb8
Stepan Avetisyan <avetisyanstepandev@gmail.com>
2021-11-22 16:37:31 +0400

    initional commit

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -17,24 +17,24 @@ export const Events = ({eventsData}:EventsProps) => {
             <div className='md:flex-row flex flex-col  mt-24'>
                 {eventsData?.data.map((event,key) => (
                     <div    
-                        key={`${event.organizer}+${key}`} 
+                        key={`${event?.organizer}+${key}`} 
                         className='flex flex-col shadow-xl md:mr-4 mx-4 md:mb-0 mb-4 rounded-lg bg-blue cursor-pointer transform duration-500 translate hover:opacity-90 hover:scale-105'>
                         <div className="flex h-64 object-cover">
                             <img src={event?.screenshot} className='rounded-t-lg' />
                         </div>   
-                        <span className='text-center mt-4 text-white text-2xl font-semibold'>{event.type}</span>
+                        <span className='text-center mt-4 text-white text-2xl font-semibold'>{event?.type}</span>
 
                         <span className='text-center mt-4 text-gray text-xl font-semibold'>Orgainaizer: 
                             <span className='text-2xl ml-2 text-white'>
-                                 {event.organizer}
+                                 {event?.organizer}
                             </span>
                         </span>
-                        <span className='text-center text-xl mt-10 ml-5 text-white'>{event.title}</span>
+                        <span className='text-center text-xl mt-10 ml-5 text-white'>{event?.title}</span>
 
-                        <span className='mt-10 ml-5 text-gray'>Start-date : {event.start_date}</span>
-                        <span className='mt-2 ml-5 text-gray'>End:date{event.end_date}</span>
-                        <span className='ml-5 mt-7 text-gray'>{event.country === '' ? "" : 'Country:'} {event.country || ''}</span>
-                        <span className='ml-5 pb-32 text-gray'>{event.city === '' ? "" : 'City:'} {event.city}</span>
+                        <span className='mt-10 ml-5 text-gray'>Start-date : {event?.start_date}</span>
+                        <span className='mt-2 ml-5 text-gray'>End:date{event?.end_date}</span>
+                        <span className='ml-5 mt-7 text-gray'>{event?.country === '' ? "" : 'Country:'} {event?.country || ''}</span>
+                        <span className='ml-5 pb-32 text-gray'>{event?.city === '' ? "" : 'City:'} {event?.city}</span>
                     </div>    
                 ))}
             </div>
